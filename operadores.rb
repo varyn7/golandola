96a9993a4de2ee1f90dc05ab60dd97bf29a92445
varyn7 <joaojaimesampaio7@gmail.com>
2026-03-03 11:15:19 -0300

    Add files via upload
    estudo sobre os operadores matemáticos de ruby e concatenando variaveis para calculo

---
+++ operadores.rb
@@ -0,0 +1,34 @@
+#operadores matemáticos
+puts 10 + 10
+var1= 10
+var2= 10
+puts var1 + var2
+#subtração
+puts 20-10
+var1= 100
+var2= 90
+puts var1 - var2
+#multiplicação
+puts 2*4
+var1= 22
+var2=22
+puts var1 * var2
+#divisão
+puts 4/2
+var1=4
+var2=2
+puts var1 / var2
+# operador de módulo
+puts 10%4
+var1=10
+var2=4
+puts var1 % var2
+#exponencial
+puts 2**2
+var1=2
+var2=2
+puts var1 ** var2
+#printando os tipos das variaveis 
+puts "var1: #{var1} | tipo: #{var1.class} | object_id: #{var1.object_id}"
+puts "var2: #{var2.class} | tipo: #{var2.class}"
+puts "var1: #{var1.class}"
