4532c157e9c31a606c1a9e5c9ee0b483718fe5de
varyn7 <joaojaimesampaio7@gmail.com>
2026-03-11 14:03:47 -0300

    Add files via upload
    estudo aprofundado de classes em ruby orientada por objetos

---
+++ computer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Computer
+  def turn_on
+    "turn on the computer"
+  end
+
+  def shutdown
+    "shutdown the computer"
+  end
+end
+
+computer = Computer.new
+puts computer.turn_on
+puts computer.shutdown
+
