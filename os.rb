cacf1f7d5c00a1b33be7259d6d18744702225f9b
varyn7 <joaojaimesampaio7@gmail.com>
2026-03-11 10:19:34 -0300

    Add files via upload
    estudo prático de gems em ruby, uso da biblioteca "os" como exemplo prático

---
+++ os.rb
@@ -0,0 +1,17 @@
+﻿# encoding: UTF-8
+
+require 'os'
+
+def my_os
+  if OS.windows?
+    "windows"
+  elsif OS.linux?
+    "linux"
+  elsif OS.mac?
+    "osx"
+  else
+    "não consegui identificar"
+  end
+end
+
+puts "meu pc possui #{OS.cpu_count} cores, é #{OS.bits} bits, e o sistema operacional é #{my_os}"
